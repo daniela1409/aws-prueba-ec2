f2c87832a83c40974fdd0532effc212b46c63ef7
Yenifer Daniela Osorio Gomez <yenifero@titamedia.com>
2023-03-06 11:32:01 -0500

    cambio puerto

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send("Hello world"));
 
-app.listen(3000);
-console.log("server on port 3000");
+app.listen(8080);
+console.log("server on port 8080");
